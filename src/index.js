9d974dd407da1e46dac26c8efedfc5d58f7504fa
pratham-svg <prathampanchariya@gmail.com>
2023-03-09 16:09:19 +0530

    All the folder and file are added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+//__________________________ Import : Necessary Package and Modules ___________________________________________
 const express = require('express')
 const multer = require('multer')
 const mongoose = require('mongoose')
 const url = 'mongodb+srv://Pratham_Panchariya:shree79766@cluster0.yd3rrae.mongodb.net/knovator_technologies'
 const app = express()
 const routes  =  express.Router()
-const port = 3000
+const port = process.env.PORT || 3000
 // app.use(app().multer())
 app.use(express.json());
-mongoose.connect(url).then(()=> console.log('Mongo Db is Connected'))
+
+//__________________________ Connection : MongoDB Cluster ___________________________________________
+mongoose.connect(url ).then(()=> console.log('Mongo Db is Connected'))
 app.use('/' , routes)
 
 
+//__________________________ Listen : Port ___________________________________________
 
 app.listen(port ,()=> { console.log(`Port is Connected at ${port}`) })
